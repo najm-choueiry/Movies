ff42985a4469d02ffcf603a4d17ae16e093c97fc
najm-choueiry <nachoueiry@outlook.com>
2024-01-02 20:50:09 +0200

    <> Setting up the homescreen page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
+const ios = Platform.OS == "ios";
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text> Home Screen</Text>
-    </SafeAreaView>
+    <>
+      <View className="flex-1 bg-neutral-800">
+        {/* search bar and logo */}
+        <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
+          <StatusBar style="light" />
+          <Text>Hello</Text>
+        </SafeAreaView>
+      </View>
+    </>
   );
 }
