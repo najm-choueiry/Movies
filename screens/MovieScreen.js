25cd5e4766fb47fbde3aa12f120a75129a647d43
najm-choueiry <nachoueiry@outlook.com>
2024-01-08 21:26:13 +0200

    <> Fixing the Chevronlefticon

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,10 +1,46 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Dimensions,
+  Platform,
+} from "react-native";
+import React, { useEffect } from "react";
+import { useRoute } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import { styles } from "../theme";
+
+var { width, height } = Dimensions.get("window");
+const ios = Platform.OS == "ios";
+const topMargin = ios ? "absolute" : "mt-3";
 
 export default function MovieScreen() {
+  const { params: item } = useRoute();
+
+  useEffect(() => {
+    // call movie
+  }, [item]);
+
   return (
-    <View>
-      <Text>MovieScreen</Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={{ paddingBottom: 20 }}
+      className="flex-1 bg-neutral-900"
+    >
+      {/* back button and movie poster */}
+      <View className="w-full ">
+        <SafeAreaView
+          className={`z-20 w-full flex-row justify-between items-center px-4 ${topMargin}`}
+        >
+          <TouchableOpacity
+            style={styles.background}
+            className="rounded-xl p-1"
+          >
+            <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+          </TouchableOpacity>
+        </SafeAreaView>
+      </View>
+    </ScrollView>
   );
 }
