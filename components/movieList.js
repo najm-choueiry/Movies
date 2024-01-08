22b1fb7145db80bf66e08063cd1ecd8a6a9b6d72
najm-choueiry <nachoueiry@outlook.com>
2024-01-08 19:06:10 +0200

    <> Bug fixes

--- components/movieList.js
+++ components/movieList.js
@@ -1,8 +1,21 @@
-import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Image,
+  Dimensions,
+} from "react-native";
 import React from "react";
 import { styles } from "../theme";
+import { useNavigation } from "@react-navigation/native";
 
-export default function MovieList({ title, data }) {
+var { width, height } = Dimensions.get("window");
+
+export default function MovieList({ title }) {
+  const navigation = useNavigation();
+  const data = [1, 2, 3, 4, 5, 6];
   return (
     <View className="mb-8 space-y-4">
       <View className="mx-4 flex-row justify-between items-center">
@@ -13,6 +26,29 @@ export default function MovieList({ title, data }) {
           </Text>
         </TouchableOpacity>
       </View>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+      >
+        {data.map((item, index) => {
+          return (
+            <TouchableWithoutFeedback
+              key={index}
+              onPress={() => navigation.navigate("Movie", item)}
+            >
+              <View className="space-y-1 mr-4">
+                <Image
+                  source={require("../assets/images/moviePoster2.png")}
+                  className="rounded-3xl"
+                  style={{ width: width * 0.33, height: height * 0.22 }}
+                />
+                <Text className="text-neutral-300 ml-1">name</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
