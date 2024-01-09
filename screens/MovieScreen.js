d75b11f61674761a3abcbf56245a6133bb0e1f93
najm-choueiry <nachoueiry@outlook.com>
2024-01-09 18:08:47 +0200

    <> Adding the back button and favorite

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -6,11 +6,12 @@ import {
   Dimensions,
   Platform,
 } from "react-native";
-import React, { useEffect } from "react";
-import { useRoute } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ChevronLeftIcon } from "react-native-heroicons/outline";
-import { styles } from "../theme";
+import { HeartIcon } from "react-native-heroicons/solid";
+import { styles, theme } from "../theme";
 
 var { width, height } = Dimensions.get("window");
 const ios = Platform.OS == "ios";
@@ -18,6 +19,10 @@ const topMargin = ios ? "absolute" : "mt-3";
 
 export default function MovieScreen() {
   const { params: item } = useRoute();
+
+  const navigation = useNavigation();
+
+  const [isFavorite, toggleFavorite] = useState(false);
 
   useEffect(() => {
     // call movie
@@ -34,10 +39,17 @@ export default function MovieScreen() {
           className={`z-20 w-full flex-row justify-between items-center px-4 ${topMargin}`}
         >
           <TouchableOpacity
+            onPress={() => navigation.goBack()}
             style={styles.background}
             className="rounded-xl p-1"
           >
             <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => toggleFavorite(!isFavorite)}>
+            <HeartIcon
+              size="35"
+              color={isFavorite ? theme.background : "white"}
+            />
           </TouchableOpacity>
         </SafeAreaView>
       </View>
