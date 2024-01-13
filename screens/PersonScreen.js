5a2eb02b6a10f9fb2b5b7066e50bfc8e1512ed25
najm-choueiry <nachoueiry@outlook.com>
2024-01-13 13:52:03 +0200

    <> Bug fixes

--- screens/PersonScreen.js
+++ screens/PersonScreen.js
@@ -6,18 +6,19 @@ import {
   ScrollView,
   SafeAreaView,
   TouchableOpacity,
+  Image,
 } from "react-native";
 import React, { useState } from "react";
 import { ChevronLeftIcon } from "react-native-heroicons/outline";
 import { HeartIcon } from "react-native-heroicons/solid";
 
-import { styles, theme } from "../theme";
+import { styles } from "../theme";
 import { useNavigation } from "@react-navigation/native";
 
 var { width, height } = Dimensions.get("window");
 
 const ios = Platform.OS == "ios";
-const verticalMargin = ios ? " " : "my-14";
+const verticalMargin = ios ? "w-11/12 ml-4" : "mt-14 mb-4  w-full";
 
 export default function PersonScreen() {
   const [isFavorite, toggleFavorite] = useState(false);
@@ -31,7 +32,7 @@ export default function PersonScreen() {
     >
       {/* back button */}
       <SafeAreaView
-        className={`z-20 w-full flex-row justify-between items-center px-4 ${verticalMargin}`}
+        className={`z-20 flex-row  justify-between items-center px-4 ${verticalMargin}`}
       >
         <TouchableOpacity
           onPress={() => navigation.goBack()}
@@ -45,6 +46,29 @@ export default function PersonScreen() {
           <HeartIcon size="35" color={isFavorite ? "red" : "white"} />
         </TouchableOpacity>
       </SafeAreaView>
+
+      {/* person details */}
+      <View>
+        <View
+          className="flex-row justify-center"
+          style={{
+            shadowColor: "gray",
+            shadowRadius: 40,
+            shadowOffset: { width: 0, height: 5 },
+            shadowOpacity: 1,
+          }}
+        >
+          <View
+            className="items-center rounded-full overflow-hidden h-72 w-72 border-2 border-neutral-500
+        "
+          >
+            <Image
+              source={require("../assets/images/castImage2.png")}
+              style={{ height: height * 0.43, width: width * 0.74 }}
+            />
+          </View>
+        </View>
+      </View>
     </ScrollView>
   );
 }
