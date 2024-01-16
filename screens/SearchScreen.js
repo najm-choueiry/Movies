171fb963ef23cead62f6e3148fa4bd981b905f06
najm-choueiry <nachoueiry@outlook.com>
2024-01-16 16:30:15 +0200

    <> Implementing the search bar

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -9,13 +9,16 @@ import {
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { XMarkIcon } from "react-native-heroicons/outline";
+import { useNavigation } from "@react-navigation/native";
 
 var { width, height } = Dimensions.get("window");
 const ios = Platform.OS == "ios";
 
-const TopMargin = ios ? " " : "mt-12";
+const TopMargin = ios ? " " : "mt-2";
 
 export default function SearchScreen() {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView className="bg-neutral-800 flex-1">
       <View
@@ -25,14 +28,13 @@ export default function SearchScreen() {
           placeholder="Search Movie"
           placeholderTextColor={"lightgray"}
           className={`pb-1 pl-6 flex-1 text-base font-semibold text-white tracking-wider`}
+        />
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Home")}
+          className="rounded-full p-3 m-1 bg-neutral-500"
         >
-          <TouchableOpacity
-            onPress={() => {}}
-            className="rounded-full p-3 m-1 bg-neutral-500"
-          >
-            <XMarkIcon size="25" color="white" />
-          </TouchableOpacity>
-        </TextInput>
+          <XMarkIcon size="25" color="white" />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
