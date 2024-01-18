fc21042f0e56a78a8275dc1751c81c2b5398f437
najm-choueiry <nachoueiry@outlook.com>
2024-01-18 17:27:10 +0200

    <> Adding loading to the movie screen

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -33,102 +33,99 @@ export default function MovieScreen() {
   const [cast, setCast] = useState([1, 2, 3, 4, 5, 6]);
 
   const [similarMovies, setSimilarMovies] = useState([1, 2, 3, 4, 5, 6]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // call movie
   }, [item]);
 
-  return (
+  return loading ? (
+    <View className="flex-1 bg-neutral-900">
+      <Loading />
+    </View>
+  ) : (
     <ScrollView
       contentContainerStyle={{ paddingBottom: 20 }}
       className="flex-1 bg-neutral-900"
     >
       {/* back button and movie poster */}
-      {loading ? (
-        <Loading />
-      ) : (
-        <>
-          <View className="w-full ">
-            <View>
-              <Image
-                source={require("../assets/images/moviePoster2.png")}
-                style={{ width, height: height * 0.65 }}
-              />
-              <LinearGradient
-                colors={[
-                  "transparent",
-                  "rgba(23,23,23,0.8)",
-                  "rgba(23,23,23,1)",
-                ]}
-                style={{ width, height: height * 0.4 }}
-                start={{ x: 0.5, y: 0 }}
-                end={{ x: 0.5, y: 1 }}
-                className="bottom-0 absolute"
-              />
-            </View>
-            <SafeAreaView
-              className={`z-20 w-full flex-row justify-between items-center px-4 ${topMargin}`}
+
+      <>
+        <View className="w-full ">
+          <View>
+            <Image
+              source={require("../assets/images/moviePoster2.png")}
+              style={{ width, height: height * 0.65 }}
+            />
+            <LinearGradient
+              colors={["transparent", "rgba(23,23,23,0.8)", "rgba(23,23,23,1)"]}
+              style={{ width, height: height * 0.4 }}
+              start={{ x: 0.5, y: 0 }}
+              end={{ x: 0.5, y: 1 }}
+              className="bottom-0 absolute"
+            />
+          </View>
+          <SafeAreaView
+            className={`z-20 w-full flex-row justify-between items-center px-4 ${topMargin}`}
+          >
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={styles.background}
+              className="rounded-xl p-1"
             >
-              <TouchableOpacity
-                onPress={() => navigation.goBack()}
-                style={styles.background}
-                className="rounded-xl p-1"
-              >
-                <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
-              </TouchableOpacity>
+              <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+            </TouchableOpacity>
 
-              <TouchableOpacity onPress={() => toggleFavorite(!isFavorite)}>
-                <HeartIcon
-                  size="35"
-                  color={isFavorite ? theme.background : "white"}
-                />
-              </TouchableOpacity>
-            </SafeAreaView>
-          </View>
+            <TouchableOpacity onPress={() => toggleFavorite(!isFavorite)}>
+              <HeartIcon
+                size="35"
+                color={isFavorite ? theme.background : "white"}
+              />
+            </TouchableOpacity>
+          </SafeAreaView>
+        </View>
 
-          <View style={{ marginTop: -height * 0.09 }} className="space-y-3">
-            <Text className="text-white text-center text-3xl font-bold tracking-wider">
-              {movieName}
-            </Text>
+        <View style={{ marginTop: -height * 0.09 }} className="space-y-3">
+          <Text className="text-white text-center text-3xl font-bold tracking-wider">
+            {movieName}
+          </Text>
 
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Released • 2020 • 180 mins
+          </Text>
+
+          {/* genres */}
+          <View className="flex-row justify-center mx-4 space-x-2">
             <Text className="text-neutral-400 font-semibold text-base text-center">
-              Released • 2020 • 180 mins
+              Action •
             </Text>
-
-            {/* genres */}
-            <View className="flex-row justify-center mx-4 space-x-2">
-              <Text className="text-neutral-400 font-semibold text-base text-center">
-                Action •
-              </Text>
-              <Text className="text-neutral-400 font-semibold text-base text-center">
-                Thrill •
-              </Text>
-              <Text className="text-neutral-400 font-semibold text-base text-center">
-                Comedy
-              </Text>
-            </View>
-
-            {/* description */}
-            <Text className="text-neutral-400 mx-4 tracking-wide">
-              loran lorem lorem lorem loran lorem lorem lorem loran lorem lorem
-              lorem loran lorem lorem lorem loran lorem lorem lorem loran lorem
-              lorem lorem loran lorem lorem lorem loran lorem lorem lorem loran
-              lorem lorem lorem loran lorem lorem lorem loran lorem lorem lorem
-              loran lorem lorem lorem loran lorem lorem lorem loran lorem lorem
-              lorem loran lorem lorem lorem loran lorem lorem lorem loran lorem
+            <Text className="text-neutral-400 font-semibold text-base text-center">
+              Thrill •
+            </Text>
+            <Text className="text-neutral-400 font-semibold text-base text-center">
+              Comedy
             </Text>
           </View>
 
-          <Cast cast={cast} navigation={navigation} />
+          {/* description */}
+          <Text className="text-neutral-400 mx-4 tracking-wide">
+            loran lorem lorem lorem loran lorem lorem lorem loran lorem lorem
+            lorem loran lorem lorem lorem loran lorem lorem lorem loran lorem
+            lorem lorem loran lorem lorem lorem loran lorem lorem lorem loran
+            lorem lorem lorem loran lorem lorem lorem loran lorem lorem lorem
+            loran lorem lorem lorem loran lorem lorem lorem loran lorem lorem
+            lorem loran lorem lorem lorem loran lorem lorem lorem loran lorem
+          </Text>
+        </View>
 
-          <MovieList
-            title="Similar Movies"
-            data={similarMovies}
-            hideSeeAll={true}
-          />
-        </>
-      )}
+        <Cast cast={cast} navigation={navigation} />
+
+        <MovieList
+          title="Similar Movies"
+          data={similarMovies}
+          hideSeeAll={true}
+        />
+      </>
     </ScrollView>
   );
 }
