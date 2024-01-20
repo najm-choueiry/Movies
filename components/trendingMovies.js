d5b80e0e9fd352a005222a135463dd4fc416d1dc
najm-choueiry <nachoueiry@outlook.com>
2024-01-20 04:34:54 +0200

    <> Displaying the posters for the trending movies

--- components/trendingMovies.js
+++ components/trendingMovies.js
@@ -9,6 +9,7 @@ import React from "react";
 
 import Carousel from "react-native-snap-carousel";
 import { useNavigation } from "@react-navigation/native";
+import { image500 } from "../api/moviedb";
 
 var { width, height } = Dimensions.get("window");
 
@@ -41,7 +42,7 @@ const MovieCard = ({ item, handleClick }) => {
   return (
     <TouchableWithoutFeedback onPress={() => handleClick(item)}>
       <Image
-        source={require("../assets/images/moviePoster1.png")}
+        source={{ uri: image500(item.poster_path) }}
         style={{ width: width * 0.6, height: height * 0.6 }}
         className="rounded-3xl"
       />
